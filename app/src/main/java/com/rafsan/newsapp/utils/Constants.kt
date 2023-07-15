83caf083444ab93e0b382b1bdbe305ae8a0025b7
Aardhya305 <126074658+Aardhya305@users.noreply.github.com>
2023-07-15 13:27:46 +0530

    15-07-23

---
+++ app/src/main/java/com/rafsan/newsapp/utils/Constants.kt
@@ -0,0 +1,19 @@
+/*
+ * *
+ *  * Created by Rafsan Ahmad on 9/27/21, 5:30 PM
+ *  * Copyright (c) 2021 . All rights reserved.
+ *
+ */
+
+package com.rafsan.newsapp.utils
+
+class Constants {
+    companion object {
+        const val API_KEY = "1c18b217deea4e369a758c84ef525334"
+        const val BASE_URL = "https://newsapi.org"
+        const val searchTimeDelay = 500L
+        const val QUERY_PER_PAGE = 10
+        const val DEFAULT_PAGE_INDEX = 1
+        const val CountryCode = "us"
+    }
+}
